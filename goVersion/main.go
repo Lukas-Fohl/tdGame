4e8c82ee695cbb1ab1211bbcf9f7938bf3e7562e
Lukas-Fohl <Lukas@Fohl.net>
2024-11-09 20:48:39 +0100

    added tower interaction for go

--- goVersion/main.go
+++ goVersion/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type play struct {
 	money      int
@@ -35,11 +38,30 @@ func etk_init(positionIn vec2, radiusIn float64, healthIn float64, rewardIn int,
 }
 
 type tower struct {
-	positionX float64
-	positionY float64
-	dmgRange  float64
-	dmg       float64
-	price     int
+	position vec2
+	dmgRange float64
+	dmg      float64
+	price    int
+}
+
+func tower_init(positionIn vec2, dmgRangeIn float64, dmgIn float64, priceIn int) tower {
+	return tower{
+		position: positionIn,
+		dmgRange: dmgRangeIn,
+		dmg:      dmgIn,
+		price:    priceIn,
+	}
+}
+
+func tower_dmgToETKList(towerIn tower, etkList [](*etk)) {
+	for i := 0; i < len(etkList); i++ {
+		disX := math.Abs(towerIn.position.x - etkList[i].position.x)
+		disY := math.Abs(towerIn.position.y - etkList[i].position.y)
+		if math.Sqrt(disX*disX+disY*disY) <= towerIn.dmgRange {
+			etkList[i].health -= towerIn.dmg
+			fmt.Println("hit")
+		}
+	}
 }
 
 type path struct {
@@ -63,12 +85,24 @@ func etk_poisitionFromPath(etkIn *etk, pathIn path) {
 		currentPercFull += vecStepInPerc
 		etkIn.position.x = pathIn.wayPoint[arrayPoisition].x + (vectorMoveX * (currentPercFull / vecStepInPerc))
 		etkIn.position.y = pathIn.wayPoint[arrayPoisition].y + (vectorMoveY * (currentPercFull / vecStepInPerc))
-		//currentVecPerc := (arrayPoisition / len(pathIn.wayPoint))
 	}
 }
 
 func main() {
+	etkList := [](*etk){}
+
 	myEtk := etk_init(vec_init(0.0, 0.0), 0.0, 1.0, 0.0, 0.0)
+	myEtk2 := etk_init(vec_init(0.0, 0.0), 0.0, 1.0, 0.0, 0.0)
+	etkList = append(etkList, &myEtk)
+	etkList = append(etkList, &myEtk2)
+
+	towerList := [](tower){}
+
+	towerList1 := tower_init(vec_init(10.0, 9.0), 5.0, 20.0, 10)
+	towerList2 := tower_init(vec_init(10.0, 9.0), 5.0, 20.0, 10)
+	towerList = append(towerList, towerList1)
+	towerList = append(towerList, towerList2)
+
 	myPath := path_init([]vec2{
 		vec_init(0.0, 0.0),
 		vec_init(5.0, 5.0),
@@ -83,16 +117,51 @@ func main() {
 	})
 
 	for i := 0; i < 1000; i++ {
-		myEtk.wayPointPerc += 0.1
-		etk_poisitionFromPath(&myEtk, myPath)
-		fmt.Print(myEtk.position.x)
-		fmt.Print(" ; ")
-		fmt.Println(myEtk.position.y)
+		for j := 0; j < len(etkList); j++ {
+			etkList[j].wayPointPerc += 0.1
+			etk_poisitionFromPath(etkList[j], myPath)
+			//fmt.Print(etkList[j].position.x)
+			//fmt.Print(" ; ")
+			//fmt.Println(etkList[j].position.y)
+		}
+		for j := 0; j < len(towerList); j++ {
+			tower_dmgToETKList(towerList[j], etkList)
+		}
+
+		listToRemove := []int{}
+
+		for j := len(etkList) - 1; j >= 0; j-- {
+			if etkList[j].health <= 0.0 || etkList[j].wayPointPerc >= 100.0 {
+				listToRemove = append(listToRemove, j)
+			}
+		}
+		for k := len(listToRemove) - 1; k >= 0; k-- {
+			idx := listToRemove[k]
+			if idx+1 >= len(etkList) {
+				etkList = etkList[:len(etkList)-1]
+			} else {
+				etkList = append(etkList[:idx], etkList[idx+1:]...)
+			}
+		}
+		//remove etk from list
+		//-> give mone
 	}
 	return
 }
 
 /*
+
+ablauf:
+	etk
+		tick
+			move
+			check for full path -> kys
+			check for health -> kys + money
+	tower
+		check for all in range
+		-> apply dmg
+	repead on emtpy etk list
+
 Idee:
 	liste von etks:
 		helth
@@ -109,4 +178,7 @@ Idee:
 		liste an punkten
 		--> interpolate
 
+TODO:
+	restrict tower hit am
+	restrict etk movement
 */
